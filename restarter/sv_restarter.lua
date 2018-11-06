9ad786e1a87cc9425d1aeb54f53ad0100aa87662
JHodgson1 <hodgsonjack@hotmail.com>
2018-11-06 13:25:02 +0000

    added a 'duration' variable, in minutes. added some comments, letting the client know what is happening where. changed the variable 'resource-name' to 'res_name' because i'm dumb

--- restarter/sv_restarter.lua
+++ restarter/sv_restarter.lua
@@ -2,18 +2,20 @@ local success = false
 local success2 = false
 local success3 = false
 
-local resource-name = 'resource' -- change this
+local res_name = 'resource' -- change this
+
+local duration = 5
 
 Citizen.CreateThread(function()
 	while true do
-		Citizen.Wait(600 * 1000) --600 seconds * milliseconds = 10 mins | change if you wish.
-		success = StopResource(resource-name)
-		if (success) then
-			success = StartResource(resource-name)
-			if (success) then
-				TriggerClientEvent('chatMessage', -1, "", {255, 0, 0}, "^1Resource \"" .. resourcename .. "\" has been restarted.") -- Message that announces to all players once it has been restarted.
+		Citizen.Wait(duration * 60000) -- duration(mins) * 60000 = duration(mins) in ms | change the variable 'duration' if you want to change it.
+		success = StopResource(res_name)
+		if (success) then -- if the resource was stopped then
+			success = StartResource(res_name) -- change 'success' to starting the resource
+			if (success) then -- if the resource starts again
+				TriggerClientEvent('chatMessage', -1, "", {255, 0, 0}, "^1Resource \"" .. res_name .. "\" has been restarted.") -- Message that announces to all players once it has been restarted.
 			else
-				TriggerClientEvent('chatMessage', -1, "", {255, 0, 0}, "Failed to restart \"" .. resourcename .. "\".") -- If the resource fails to restart then a message will be sent to all players.
+				TriggerClientEvent('chatMessage', -1, "", {255, 0, 0}, "Failed to restart \"" .. res_name .. "\".") -- If the resource fails to restart then a message will be sent to all players.
 			end
 		end
 	end
